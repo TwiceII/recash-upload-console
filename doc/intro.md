af32220a9d001bdbcf34bf03a11038e3bd8ccda8
Twice_ii <alish1309@rambler.ru>
2017-08-18 22:22:22 +0600

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to recash-upload-console
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/recash_upload_console/core_test.clj
@@ -0,0 +1,7 @@
+(ns recash-upload-console.core-test
+  (:require [clojure.test :refer :all]
+            [recash-upload-console.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
